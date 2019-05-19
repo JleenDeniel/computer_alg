f1930e8b000b9e634ad3de82c503bde935bb990c
maksiplus19 <maksiplus19@gmail.com>
2019-05-19 16:29:54 +0300

    lab04.py updated. del_zero() replaced by library func trim(). Delete useless comments

--- lab04/lab04.py
+++ lab04/lab04.py
@@ -1,26 +1,15 @@
-from typing import Union, Optional
+from typing import Union
 import numpy as np
 from numpy.polynomial.polynomial import Polynomial as Poly
 from lab01.circle import InverseMod
 
 
 def rev(p: Poly, n: int) -> Poly:
-    p = del_zero(p)
+    p = p.trim()
     result = p.coef[::-1]
     if len(p.coef) < n:
         result = np.concatenate([np.zeros(n - len(p.coef)), result])
     return Poly(result)
-
-
-def del_zero(p: Optional[Poly]) -> Poly:
-    return p.trim()
-    # count = 0
-    # for i in p.coef[::-1]:
-    #     if abs(i) < 10 ** -5:
-    #         count += 1
-    #     else:
-    #         break
-    # return p.truncate(len(p.coef) - count) if len(p.coef) != count else Poly([0])
 
 
 def PolyInverseModOverQ(f: Poly, mod_deg: int) -> Union[Poly, None]:
@@ -45,7 +34,6 @@ def PolyInverseModOverZn(f: Poly, mod_deg: int, mod_ring: int) -> Union[Poly, No
     if f.coef[0] == 0 or mod_ring == 1:
         return None
 
-    # f.coef = np.array([int(i % mod_ring) for i in f.coef], int)
     f = Poly(np.mod(f.coef, mod_ring))
 
     c = InverseMod(f.coef[0], mod_ring)
@@ -67,9 +55,8 @@ def PolyDivModOverQ(a: Poly, b: Poly) -> (Poly, Poly):
     if not b.coef.any():
         raise ZeroDivisionError
 
-    a = del_zero(a)
-
-    b = del_zero(b)
+    a = a.trim()
+    b = b.trim()
     n, m = len(a.coef), len(b.coef)
 
     if n < m:
@@ -84,8 +71,8 @@ def PolyDivModOverQ(a: Poly, b: Poly) -> (Poly, Poly):
             q.coef = np.concatenate([np.zeros(n - len(q)), q.coef])
 
         r = a - b * q
-        r = del_zero(r)
-        q = del_zero(q)
+        r = r.trim()
+        q = r.trim()
         return q, r
 
 
@@ -99,22 +86,14 @@ def PolyDivModOverZn(a: Poly, b: Poly, mod_r: int) -> (Poly, Poly):
     a = Poly(np.mod(a.coef, mod_r))
     b = Poly(np.mod(b.coef, mod_r))
 
-    a = del_zero(a)
-
-    # deleted_zero = 0
-    # for i in b.coef[::-1]:
-    #     if abs(i) < 10 ** -10:
-    #         deleted_zero += 1
-    #     else:
-    #         break
-
-    b = del_zero(b)
+    a = a.trim()
+    b = b.trim()
     n, m = len(a.coef), len(b.coef)
 
     if n < m:
         return Poly([0]), a
     else:
-        f = rev(b, m) # - deleted_zero)
+        f = rev(b, m)
         g = PolyInverseModOverZn(f, n - m + 1, mod_r)
 
         if g is None:
@@ -131,6 +110,6 @@ def PolyDivModOverZn(a: Poly, b: Poly, mod_r: int) -> (Poly, Poly):
         r = a - bq
         r = Poly(np.mod(r.coef, mod_r))
         q = Poly(np.mod(q.coef, mod_r))
-        r = del_zero(r)
-        q = del_zero(q)
+        r = r.trim()
+        q = q.trim()
         return q, r
